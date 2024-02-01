ba4e5f9f75ae43252b803761dcaaf0f38c276147
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-02-01 07:43:24 +0100

    learing useEffect basics

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,20 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import { useRef, useEffect } from "react";
+import { createPortal } from "react-dom";
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+export default function Modal({ open, children }) {
   const dialog = useRef();
-
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [])
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog} open={open}>
       {children}
     </dialog>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
-});
-
-export default Modal;
+}
